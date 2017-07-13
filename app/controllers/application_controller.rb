6926759e2542f2a0397489ed4fad01ba23ecdbba
Gerrald Mitchell <bluelionjr@gmail.com>
2017-07-13 12:13:33 -0400

    Views and paths all working

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,18 @@ class ApplicationController < ActionController::Base
   before_action :authenticate_user!, :except => [:index]
   before_filter :configure_permitted_parameters, if: :devise_controller?
 
+    def after_sign_in_path_for(resource)
+      if current_user.poll
+        poll_path(current_user.poll)
+      else
+        new_poll_path
+      end
+    end
+
+    def after_sign_out_path_for(resource_or_scope)
+      request.referrer
+    end
+
   protected
     def configure_permitted_parameters
       devise_parameter_sanitizer.permit(:sign_up, keys: [:state])
