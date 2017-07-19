93dd9db57ca20e957951d6234a1f2bd372813d1d
Gerrald Mitchell <bluelionjr@gmail.com>
2017-07-18 22:32:06 -0400

    Navbar overhaul

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,4 +6,8 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :trackable, :validatable
 
 
+  def avatar_url(size)
+    gravatar_id = Digest::MD5::hexdigest(self.email).downcase
+    "http://gravatar.com/avatar/#{gravatar_id}.png?s=#{size}"
+  end
 end
